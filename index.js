e048c310f480e92450dd6585eaa59e0822770e9c
Drew Miley <drewmiley92@gmail.com>
2019-07-28 17:52:39 +0100

    Cleans up code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     scene: {
-        preload: preload,
-        create: create,
-        update: update
+        preload,
+        create,
+        update
     }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
@@ -20,11 +19,9 @@ function preload ()
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-function create ()
-{
+function create () {
     this.add.image(400, 300, 'sky');
 }
 
-function update ()
-{
+function update () {
 }
