9ef73c4fbc664c4b7b6feccbf43f4bf0e20ab9e8
Drew Miley <drewmiley92@gmail.com>
2019-07-29 23:44:34 +0100

    Default export in preload

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,12 +1,10 @@
 import assets from '../assets';
 import { AssetNames } from './constants';
 
-function preload() {
+export default function() {
     this.load.image(AssetNames.Sky, assets.Sky);
     this.load.image(AssetNames.Ground, assets.Platform);
     this.load.image(AssetNames.Star, assets.Star);
     this.load.image(AssetNames.Bomb, assets.Bomb);
     this.load.spritesheet(AssetNames.Dude, assets.Dude, { frameWidth: 32, frameHeight: 48 });
 }
-
-export default preload
